ccdd627ee5c6b9cd07cba27c2ee51d4182a190a5
Kenneth Sumang <kennethsumang08@gmail.com>
2022-02-08 16:07:28 +0800

    feat: error handling

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,7 +2,9 @@
 
 namespace App\Exceptions;
 
+use App\Constants\ErrorConstants;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Validation\ValidationException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -10,16 +12,22 @@ class Handler extends ExceptionHandler
     /**
      * A list of the exception types that are not reported.
      *
-     * @var array<int, class-string<Throwable>>
+     * @var string[]
      */
     protected $dontReport = [
-        //
+        BadRequestException::class,
+        NotFoundException::class,
+        ServerErrorException::class,
+        UnauthorizedException::class,
+        ForbiddenException::class,
+        ValidationException::class,
+        ConflictException::class
     ];
 
     /**
      * A list of the inputs that are never flashed for validation exceptions.
      *
-     * @var array<int, string>
+     * @var string[]
      */
     protected $dontFlash = [
         'current_password',
@@ -34,8 +42,81 @@ class Handler extends ExceptionHandler
      */
     public function register()
     {
-        $this->reportable(function (Throwable $e) {
-            //
+        $this->renderable(function (BadRequestException|ValidationException $e) {
+            return response()->json(
+                $this->getErrorData(
+                    ErrorConstants::ERROR_CODES[ErrorConstants::BAD_REQUEST],
+                    $e->getMessage()
+                ),
+                ErrorConstants::ERROR_CODES[ErrorConstants::BAD_REQUEST]
+            );
         });
+
+        $this->renderable(function (NotFoundException $e) {
+            return response()->json(
+                $this->getErrorData(
+                    ErrorConstants::ERROR_CODES[ErrorConstants::NOT_FOUND],
+                    $e->getMessage()
+                ),
+                ErrorConstants::ERROR_CODES[ErrorConstants::NOT_FOUND]
+            );
+        });
+
+        $this->renderable(function (ServerErrorException $e) {
+            return response()->json(
+                $this->getErrorData(
+                    ErrorConstants::ERROR_CODES[ErrorConstants::INTERNAL_SERVER_ERROR],
+                    $e->getMessage()
+                ),
+                ErrorConstants::ERROR_CODES[ErrorConstants::INTERNAL_SERVER_ERROR]
+            );
+        });
+
+        $this->renderable(function (UnauthorizedException $e) {
+            return response()->json(
+                $this->getErrorData(
+                    ErrorConstants::ERROR_CODES[ErrorConstants::UNAUTHORIZED],
+                    $e->getMessage()
+                ),
+                ErrorConstants::ERROR_CODES[ErrorConstants::UNAUTHORIZED]
+            );
+        });
+
+        $this->renderable(function (ForbiddenException $e) {
+            return response()->json(
+                $this->getErrorData(
+                    ErrorConstants::ERROR_CODES[ErrorConstants::FORBIDDEN],
+                    $e->getMessage()
+                ),
+                ErrorConstants::ERROR_CODES[ErrorConstants::FORBIDDEN]
+            );
+        });
+
+        $this->renderable(function (ConflictException $e) {
+            return response()->json(
+                $this->getErrorData(
+                    ErrorConstants::ERROR_CODES[ErrorConstants::CONFLICT],
+                    $e->getMessage()
+                ),
+                ErrorConstants::ERROR_CODES[ErrorConstants::CONFLICT]
+            );
+        });
+    }
+
+    /**
+     * Gets error data for JSON response
+     * @param int $code
+     * @param string $message
+     * @return array
+     */
+    private function getErrorData(int $code, string $message, array $data = []) : array
+    {
+        return [
+            'error' => [
+                'code' => $code,
+                'message' => $message,
+                'data' => $data
+            ]
+        ];
     }
 }
