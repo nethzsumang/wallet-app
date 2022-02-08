92a9010cf51439fe4c664ca2a65f96aed51b3ec0
Kenneth Sumang <kennethsumang08@gmail.com>
2022-02-08 17:32:27 +0800

    feat: add separate checker for unique email and username

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -2,7 +2,9 @@
 namespace App\Services;
 
 use App\Repositories\UserRepository;
+use App\Exceptions\ConflictException;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Validator;
 
 /**
  * UserService class
@@ -22,9 +24,36 @@ class UserService extends BaseService
         $this->userRepository = $userRepository;
     }
 
+    /**
+     * Register a new user
+     * @param array $data
+     * @return JsonResource
+     * @throws ConflictException
+     */
     final public function register(array $data) : JsonResource
     {
+        $this->checkUniqueFields($data);
         $user = $this->userRepository->create($data);
         return new JsonResource($user);
+    }
+
+    /**
+     * Checks unique fields in user data
+     * @param array $data
+     * @return void
+     * @throws ConflictException
+     */
+    private function checkUniqueFields(array $data) : void
+    {
+        $validator = Validator::make(
+            $data,
+            [
+                'email' => 'unique:users,email',
+                'username' => 'unique:users,username'
+            ]
+        );
+        if ($validator->fails()) {
+            throw new ConflictException($validator->errors()->first());
+        }
     }
 }
