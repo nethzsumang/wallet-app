5a6f05e787707c2fcb65bafd7fd1e94753143477
Kenneth Sumang <kennethsumang08@gmail.com>
2022-02-09 23:05:56 +0800

    test: slack notification

--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->group(static function () {
     Route::get('/', static function () {
         return response()->json([
-            'message' => 'Welcome to the API',
+            'message' => 'Welcome to the API.',
         ]);
     });
     
