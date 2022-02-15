39081b7425b61b15aa9902314eab71959aeef9b2
Kenneth Sumang <kennethsumang08@gmail.com>
2022-02-15 18:29:44 +0800

    feat: added validations

--- app/Http/Requests/Account/CreateAccountRequest.php
+++ app/Http/Requests/Account/CreateAccountRequest.php
@@ -27,7 +27,7 @@ class CreateAccountRequest extends BaseRequest
             'user_id' => ['required', 'integer'],
             'name' => ['required', 'string', 'max:50'],
             'description' => ['nullable', 'string', 'max:100'],
-            'amount' => ['required', 'numeric']
+            'amount' => ['required', 'numeric', 'min:0'],
         ];
     }
 }
--- app/Services/AccountService.php
+++ app/Services/AccountService.php
@@ -1,10 +1,12 @@
 <?php
 namespace App\Services;
 
+use App\Exceptions\NotFoundException;
 use App\Http\Resources\Account\AccountResource;
 use App\Repositories\AccountRepository;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Validator;
 
 /**
  * Class AccountService
@@ -28,12 +30,31 @@ class AccountService extends BaseService
      * Create a new account
      * @param array $data
      * @return JsonResource
+     * @throws NotFoundException
      */
     final public function createAccount(array $data): JsonResource
     {
         Arr::forget($data, 'amount');
+        $this->checkUserId($data['user_id']);
         $account = $this->accountRepository->create($data);
         $accountWithSummary = $this->accountRepository->getAccountWithSummaries($account->id);
         return new AccountResource($accountWithSummary);
+    }
+
+    /**
+     * Checks user id if it exists
+     * @param int $userId
+     * @return void
+     * @throws NotFoundException
+     */
+    private function checkUserId(int $userId): void
+    {
+        $validator = Validator::make(
+            [ 'user_id' => $userId ],
+            [ 'user_id' => 'exists:users,id' ]
+        );
+        if ($validator->fails()) {
+            throw new NotFoundException($validator->errors()->first());
+        }
     }
 }
